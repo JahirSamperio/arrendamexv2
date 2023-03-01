0a9f84ecbadf1cba45e1c3673ddc508d2eda97b9
Eivar Ulises Cuevas <putyfive@gmail.com>
2023-02-28 19:30:10 -0600

    asecta jahir por favor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <h2>Hola Jahir, un gusto</h2>
     <form action="">
       <input type="text" placeholder='jahir sorra'/>
