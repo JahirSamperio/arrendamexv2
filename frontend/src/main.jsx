d65d369a9d7417a022e689d302d73e62850c5c09
Eivar Ulises Cuevas <putyfive@gmail.com>
2023-02-28 19:05:02 -0600

    asecta por favor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,5 +7,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <h2>Hola Jahir, un gusto</h2>
+    <form action="">
+      <input type="text" placeholder='jahir sorra'/>
+    </form>
   </React.StrictMode>,
 )
