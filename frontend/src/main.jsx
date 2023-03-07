d0224d601a7be14d00ba60eba6b952ee9a27d3ad
Eivar Ulises Cuevas <putyfive@gmail.com>
2023-03-07 17:04:18 -0600

    nose k hacer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <h2>Hola Jahir, un gusto</h2>
-  </React.StrictMode>,
-)
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './router/AppRouter';
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App></App>
+);
