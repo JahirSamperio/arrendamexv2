aa9b50b5eb26c6eb7e93378c3a1b13522bdb18ca
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2023-03-06 23:13:19 -0600

    Finalizado

--- backend/src/models/login/login.js
+++ backend/src/models/login/login.js
@@ -1,13 +1,14 @@
 const {conexion} = require('../../db/config');
 
-function logUserModel(email, password){
+function logUserModel(data){
+    const {email, password} = data;
     return new Promise((resolve, reject) => {
         conexion.query(
             `SELECT * FROM usuarios u
             WHERE u.email = "${email}" 
             AND u.password = "${password}"`, 
             function (error, result, field) {
-                if(error) 
+                if(error)
                     return reject(error);
                 return resolve(result);
             })
